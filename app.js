092b97a84633909707d168df7c9b3bc028cfd207
IvanVojnic <ivojnic44@gmail.com>
2022-07-23 21:14:49 +0300

    docs: add files

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 
 const app = express()
-
+const jsonParser = express.json();
 app.use(express.static(__dirname + '/public'))
 
-app.use('/', function (request, response) {
-    response.send(request.body);
-    response.send('<h1>Главная страница</h1>')
+app.post('/app', jsonParser, function (request, response) {
+    console.log(request.body)
+    response.json(request.body) // отправляем пришедший ответ обратно
 })
 
+app.use('/', function (request, response) {
+    response.send('<h1>Главная страница</h1>')
+})
 app.listen(3000)
